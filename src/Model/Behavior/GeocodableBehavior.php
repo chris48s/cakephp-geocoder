67b65f287f1ccd3a72b127786446e1fc6311adc6
Busani <busani.moyo@prepaidmeters.com>
2024-03-18 18:36:06 +0200

    GeocobableBehavior controller fix

--- src/Model/Behavior/GeocodableBehavior.php
+++ src/Model/Behavior/GeocodableBehavior.php
@@ -70,7 +70,7 @@ class GeocodableBehavior extends Behavior
         $response = $http->get($url, $parameters);
         $response = json_decode($response->getStringBody());
 
-        if ($response->isSuccess()) {
+        if ($response->status == 'OK') {
             $entity->{$latitudeColumn} = floatval($response->results[0]->geometry->location->lat);
             $entity->{$longitudeColumn} = floatval($response->results[0]->geometry->location->lng);
 
